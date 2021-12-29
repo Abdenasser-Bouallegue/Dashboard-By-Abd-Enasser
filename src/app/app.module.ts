7f56452e89e7c0678f0d95dc14cca197735aa864
Abd enasser Bouallegue <89575303+Abdenasser-Bouallegue@users.noreply.github.com>
2021-12-29 20:30:07 +0100

    Dashboard Testing 1.0

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TabsComponent } from './tabs/tabs.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import{MatIconModule} from '@angular/material/icon';
+import { BookmarksComponent } from './bookmarks/bookmarks.component';
+import { TodosComponent } from './todos/todos.component';
+import { NotesComponent } from './notes/notes.component';
+import { BookmarkTileComponent } from './bookmark-tile/bookmark-tile.component'
+@NgModule({
+  declarations: [
+    AppComponent,
+    TabsComponent,
+    BookmarksComponent,
+    TodosComponent,
+    NotesComponent,
+    BookmarkTileComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
